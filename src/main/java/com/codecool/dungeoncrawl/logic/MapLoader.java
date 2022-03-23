7ea59a78014b04c0196bae7a23f337f598a4553b
Karolina Gliszczyńska <hello@karolinagliszczynska.com>
2022-03-23 11:44:08 +0100

    Map loader: bat cam be placed on map

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -1,5 +1,6 @@
 package com.codecool.dungeoncrawl.logic;
 
+import com.codecool.dungeoncrawl.logic.actors.Bat;
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.actors.Skeleton;
 import com.codecool.dungeoncrawl.logic.items.Item;
@@ -37,6 +38,10 @@ public class MapLoader {
                         case 's':
                             cell.setType(CellType.FLOOR);
                             new Skeleton(cell);
+                            break;
+                        case 'b':
+                            cell.setType(CellType.FLOOR);
+                            new Bat(cell);
                             break;
                         case '@':
                             cell.setType(CellType.FLOOR);
