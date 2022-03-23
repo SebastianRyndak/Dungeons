25f9429dd1e397063495b9741ea45902d4bc035a
Sebastian Ryndak <ryndaksebastiann@gmail.com>
2022-03-23 15:52:40 +0100

    fix_all

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -4,6 +4,7 @@ import com.codecool.dungeoncrawl.logic.actors.Bat;
 import com.codecool.dungeoncrawl.logic.actors.GiantSpider;
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.actors.Skeleton;
+import com.codecool.dungeoncrawl.logic.items.Door;
 import com.codecool.dungeoncrawl.logic.items.Item;
 import com.codecool.dungeoncrawl.logic.items.Key;
 import com.codecool.dungeoncrawl.logic.items.Sword;
@@ -59,6 +60,10 @@ public class MapLoader {
                         case 'k':
                             cell.setType(CellType.FLOOR);
                             new Key(cell);
+                            break;
+                        case 'd':
+                            cell.setType(CellType.DOOR);
+                            new Door(cell);
                             break;
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
