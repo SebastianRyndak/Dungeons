eae3cdef98467deea46ef9d4f41b55f00ef3ddf0
Karolina Gliszczyńska <hello@karolinagliszczynska.com>
2022-03-23 10:57:34 +0100

    class player segt strength & health

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -3,8 +3,12 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 
 public class Player extends Actor {
+
+
     public Player(Cell cell) {
         super(cell);
+        this.setStrength(5);
+        this.setHealth(15);
     }
 
     public String getTileName() {
